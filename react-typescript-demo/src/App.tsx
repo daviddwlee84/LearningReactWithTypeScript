25822b2e6aaef1522181a588ec6c5e40c76dbac4
Da-Wei Lee <daweilee@microsoft.com>
2023-02-20 12:28:10 +0800

    Introduce object type props for episode 4

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet'
+import { Person } from './components/Person';
 
 // App component
 // (type inference)
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
   return (
     <div className="App">
       <Greet name='Vishwas' messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
     </div>
   );
 }
